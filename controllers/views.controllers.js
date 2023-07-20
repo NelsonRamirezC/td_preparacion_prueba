e11fa17bb7b2ddfbe918efd57533f723fc0138e6
Nelson Ramirez <xhyskoax@gmail.com>
2023-07-20 12:59:44 -0400

    se aplica filtro por fechas

--- controllers/views.controllers.js
+++ controllers/views.controllers.js
@@ -1,13 +1,72 @@
-
-import sequelize from "../database/database.js"
+import sequelize from "../database/database.js";
+import moment from "moment";
+import { Op } from "sequelize";
 
 const home = async (req, res) => {
-    let customers = await sequelize.models.customers.findAll();
-    customers = customers.map(customer => customer.toJSON());
+    //status
+    let parametros = req.query;
 
-    res.render("home", {
-        customers
-    });
+    let filtros = {};
+
+    for (const key in parametros) {
+        if (key == "status") {
+            filtros.status = {
+                [Op.eq]: parametros[key],
+            };
+        }
+    }
+
+    try {
+        let orders = await sequelize.models.orders.findAll({
+            where: filtros,
+        });
+
+        orders = orders.map((order) => order.toJSON());
+
+        //obtener opciones de los status ingresados en la base
+        let [rows] = await sequelize.query(
+            "select distinct status from orders"
+        );
+
+        //filtrar por fecha de orden
+        if (parametros.orderdate)
+            orders = orders.filter((order) => {
+                order.orderdate = String(order.orderdate);
+                if (order.orderdate.includes(parametros.orderdate.trim())) {
+                    return order;
+                }
+            });
+        //filtro por fecha required
+        if (parametros.requireddate)
+            orders = orders.filter((order) => {
+                order.requireddate = String(order.requireddate);
+                if (
+                    order.requireddate.includes(parametros.requireddate.trim())
+                ) {
+                    return order;
+                }
+            });
+        //filtro fecha despacho
+        if (parametros.shippeddate)
+            orders = orders.filter((order) => {
+                order.shippeddate = String(order.shippeddate);
+                if (order.shippeddate.includes(parametros.shippeddate.trim())) {
+                    return order;
+                }
+        });
+
+        console.log(rows);
+        let estados = rows;
+        res.render("home", {
+            estados,
+            orders,
+        });
+    } catch (error) {
+        console.log(error);
+        res.render("home", {
+            error: "Ha ocurrido un error, intente más tarde.",
+        });
+    }
 };
 
 let controllers = {
